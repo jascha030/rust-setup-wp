267a2f3afbd6972a3a7b5ea6bc943801a5dc18c3
Jascha030 <contact@jaschavanaalst.nl>
2021-09-13 20:07:21 +0200

    Display message with hidden password

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,36 @@ struct Cli {
     password: String,
 }
 
+fn get_password_display(password: String) -> String {
+    let mut display: String = String::from("");
+
+    let mut i = 0;
+    let length = password.chars().count();
+
+    while i < length {
+        display.push_str("*");
+        i = i + 1;
+    }
+
+    return display;
+}
+
+fn output_arguments_message(args: Cli) {
+    let password_display: String = get_password_display(args.password);
+
+    println!(
+        "Creating a project with values name: {}, user: {}, pass: {}",
+        args.name,
+        args.user,
+        password_display
+    );
+}
+
 fn main() {
     let args = Cli::from_args();
+
+    // Output message to display args
+    output_arguments_message(args);
 }
 
 
