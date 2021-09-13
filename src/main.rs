bd11a3fe9f846e09fa8e95f51a551fc13bb6fe73
Jascha030 <contact@jaschavanaalst.nl>
2021-09-13 20:49:46 +0200

    Refactor password_display to be method for DatabaseCredentials impl

--- src/main.rs
+++ src/main.rs
@@ -16,44 +16,42 @@ struct DatabaseCredentials {
 impl DatabaseCredentials {
     pub fn new(cli: Cli) -> Self {
         println!("Database password: ");
-        let pass = read_password().unwrap();
+        let password = read_password().unwrap();
 
         DatabaseCredentials {
             name: String::from(cli.name),
             user: String::from(cli.database_user),
-            password: pass
+            password
         }
     }
-}
 
+    pub fn password_display(&self) -> String {
+        let mut display: String = String::from("");
 
-fn get_password_display(password: String) -> String {
-    let mut display: String = String::from("");
+        let mut i = 0;
+        let length: usize = self.password.chars().count();
 
-    let mut i = 0;
-    let length = password.chars().count();
+        while i < length {
+            display.push_str("*");
+            i = i + 1;
+        }
 
-    while i < length {
-        display.push_str("*");
-        i = i + 1;
+        return display;
     }
-
-    return display;
 }
 
 fn output_arguments_message(credentials: DatabaseCredentials) {
-    let password_display: String = get_password_display(credentials.password);
-
     println!(
         "Creating a project with values name: {}, user: {}, pass: {}",
         credentials.name,
         credentials.user,
-        password_display
+        credentials.password_display()
     );
 }
 
 fn main() {
-    let credentials: DatabaseCredentials = DatabaseCredentials::new(Cli::from_args());
+    let arguments = Cli::from_args();
+    let credentials: DatabaseCredentials = DatabaseCredentials::new(arguments);
 
     // Output message to display args
     output_arguments_message(credentials);
