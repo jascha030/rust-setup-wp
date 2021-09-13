610766a1bd4a122a99eda25b4f37a221a3bc74fd
Jascha030 <contact@jaschavanaalst.nl>
2021-09-13 20:58:30 +0200

    Also refactor output_arguments_message to be method\

--- src/main.rs
+++ src/main.rs
@@ -4,49 +4,46 @@ use rpassword::read_password;
 #[derive(StructOpt)]
 struct Cli {
     name: String,
-    database_user: String
+    database_user: String,
 }
 
 struct DatabaseCredentials {
     name: String,
     user: String,
-    password: String
+    password: String,
 }
 
 impl DatabaseCredentials {
     pub fn new(cli: Cli) -> Self {
         println!("Database password: ");
-        let password = read_password().unwrap();
 
         DatabaseCredentials {
             name: String::from(cli.name),
             user: String::from(cli.database_user),
-            password
+            password: read_password().unwrap(),
         }
     }
 
     pub fn password_display(&self) -> String {
         let mut display: String = String::from("");
-
         let mut i = 0;
-        let length: usize = self.password.chars().count();
 
-        while i < length {
+        while i < self.password.chars().count() {
             display.push_str("*");
             i = i + 1;
         }
 
         return display;
     }
-}
 
-fn output_arguments_message(credentials: DatabaseCredentials) {
-    println!(
-        "Creating a project with values name: {}, user: {}, pass: {}",
-        credentials.name,
-        credentials.user,
-        credentials.password_display()
-    );
+    pub fn output_arguments_message(&self) {
+        println!(
+            "Creating a project with values name: {}, user: {}, pass: {}",
+            self.name,
+            self.user,
+            self.password_display()
+        );
+    }
 }
 
 fn main() {
@@ -54,7 +51,7 @@ fn main() {
     let credentials: DatabaseCredentials = DatabaseCredentials::new(arguments);
 
     // Output message to display args
-    output_arguments_message(credentials);
+    credentials.output_arguments_message();
 }
 
 
