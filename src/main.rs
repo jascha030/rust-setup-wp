196c06e1f7cbd02539fdba3ce3ae3dc3b3bbdd51
Jascha030 <contact@jaschavanaalst.nl>
2021-09-13 20:03:38 +0200

    Cli struct for arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+struct Cli {
+    name: String,
+    user: String,
+    password: String,
 }
+
+fn main() {
+    let args = Cli::from_args();
+}
+
+
